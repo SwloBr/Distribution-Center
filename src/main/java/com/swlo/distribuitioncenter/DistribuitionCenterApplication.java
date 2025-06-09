aa078deda6c4042d7f88a83a0e34e59749dfe488
swlo <ppaulo2003.ppsr@gmail.com>
2025-06-09 09:05:34 -0300

    :tada:

---
+++ src/main/java/com/swlo/distribuitioncenter/DistribuitionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.swlo.distribuitioncenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistribuitionCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistribuitionCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swlo/distribuitioncenter/DistribuitionCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swlo.distribuitioncenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuitionCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
