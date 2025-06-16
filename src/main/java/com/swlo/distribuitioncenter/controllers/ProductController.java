ecc6c634150fb6305367620976138edb054d4dde
João Victor Guidi de Lima <100796909+Joao-2003@users.noreply.github.com>
2025-06-16 13:32:01 -0300

    Update ProductController.java

--- src/main/java/com/swlo/distribuitioncenter/controllers/ProductController.java
+++ src/main/java/com/swlo/distribuitioncenter/controllers/ProductController.java
@@ -1,4 +1,42 @@
 package com.swlo.distribuitioncenter.controllers;
 
+import com.swlo.distribuitioncenter.entities.ProductEntity;
+import com.swlo.distribuitioncenter.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductEntity> createProduct(@RequestBody ProductEntity product) {
+        return ResponseEntity.ok(productService.createProduct(product));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductEntity> getProduct(@PathVariable String id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductEntity>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductEntity> updateProduct(@PathVariable String id, @RequestBody ProductEntity product) {
+        return ResponseEntity.ok(productService.updateProduct(id, product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
 }
