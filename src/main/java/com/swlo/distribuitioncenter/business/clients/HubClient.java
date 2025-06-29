23f4e6ac73040bf656c65cc535e1a3312239bb85
swlo <ppaulo2003.ppsr@gmail.com>
2025-06-29 15:59:38 -0300

    :sparkles: feat: update HubClient to use QuotationResponseDto and modify endpoint signatures

--- src/main/java/com/swlo/distribuitioncenter/business/clients/HubClient.java
+++ src/main/java/com/swlo/distribuitioncenter/business/clients/HubClient.java
@@ -2,31 +2,27 @@ package com.swlo.distribuitioncenter.business.clients;
 
 
 import com.swlo.distribuitioncenter.business.dto.CdDto;
-import com.swlo.distribuitioncenter.business.dto.ProductDto;
+import com.swlo.distribuitioncenter.business.dto.QuotationResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.HashMap;
+import java.util.List;
 
+@Component
 @FeignClient(name= "hubClient", url = "${hub.address}")
 public interface HubClient {
 
     @GetMapping("/product/{productName}")
     String getProductId(@PathVariable("productName") String productName);
 
-    @PostMapping("/cd")
-    void registerDistributionCenter(
-            @RequestBody CdDto cdDto
-    );
-
-    @GetMapping("/cd/all")
+    @GetMapping("/cds/disponiveis")
     CdDto[] getAllDistributionCenters();
 
     @PostMapping("/quotation/{productId}/{quantity}")
-    HashMap<String, ProductDto> getQuotation(
+    List<QuotationResponseDto> getQuotation(
             @PathVariable("productId") String productId,
             @PathVariable("quantity") Integer quantity
     );
