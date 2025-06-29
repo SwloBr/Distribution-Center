83d87ae742bbbe7741fa14cf4707707649e9fa40
swlo <ppaulo2003.ppsr@gmail.com>
2025-06-29 18:21:47 -0300

    :sparkles: feat: add DefaultController with a simple health check endpoint

---
+++ src/main/java/com/swlo/distribuitioncenter/controllers/DefaultController.java
@@ -0,0 +1,20 @@
+package com.swlo.distribuitioncenter.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaultController {
+
+    @GetMapping("")
+    public boolean getProduct() {
+        return true;
+    }
+
+
+}
